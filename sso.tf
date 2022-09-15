db3c1f465a548899ebe5f941f534c2d43c8203a5
johannes karoff <johannes@karoff.net>
2022-09-15 23:35:06 +0200

    update sso module and fork sso account-assignments

--- sso.tf
+++ sso.tf
@@ -1,6 +1,6 @@
 module "permission_sets" {
   source  = "cloudposse/sso/aws//modules/permission-sets"
-  version = "0.6.2"
+  version = "0.7.1"
 
   permission_sets = concat(
     [{
@@ -11,24 +11,26 @@ module "permission_sets" {
       tags               = {}
       inline_policy      = ""
       policy_attachments = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+      customer_managed_policy_attachments = []
     }]
   )
 }
 
 module "sso_account_assignments" {
-  source  = "cloudposse/sso/aws//modules/account-assignments"
-  version = "0.6.2"
+  source  = "github.com/cornerman/terraform-aws-sso//modules/account-assignments?ref=c3a46b35409cce9c19f2bfbf393de1331ffc7685"
 
   account_assignments = concat(
-    [for sub_account, opts in var.sub_accounts : {
-      account             = aws_organizations_account.sub_account[sub_account].id
+    [for sub_account, opts in local.sub_accounts : {
+      account_id          = aws_organizations_account.sub_account[sub_account].id
+      account_key         = sub_account
       permission_set_arn  = module.permission_sets.permission_sets[local.sso_admin_permission_set].arn
       permission_set_name = local.sso_admin_permission_set
       principal_type      = "GROUP"
       principal_name      = local.sso_admin_group_name
     }],
     [{
-      account             = data.aws_caller_identity.current.account_id
+      account_id          = data.aws_caller_identity.current.account_id
+      account_key         = data.aws_caller_identity.current.account_id
       permission_set_arn  = module.permission_sets.permission_sets[local.sso_admin_permission_set].arn
       permission_set_name = local.sso_admin_permission_set
       principal_type      = "GROUP"
@@ -36,4 +38,5 @@ module "sso_account_assignments" {
     }]
   )
 }
+
 
